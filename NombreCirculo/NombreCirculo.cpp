0845ea5742be2bf1c7857e71c6d6a3da3b4ce5d4
braad <braad@A22100190>
2022-12-03 21:37:47 -0600

    Agregar archivos de proyecto.

---
+++ NombreCirculo/NombreCirculo.cpp
@@ -0,0 +1,20 @@
+#include 	<conio.h>
+#include 	<stdio.h>
+
+int main() {
+	printf("              ***             \n");
+	printf("           *       *           \n");
+	printf("        *             *                     \n");
+	printf("     *                    *                    \n");
+	printf("   *                       *        \n");
+	printf("  *                         *               \n");
+	printf("*                             *              \n");
+	printf("*        Brandon Lopez        *              \n");
+	printf("*                             *             \n"); a
+		printf("  *                         *             \n");
+	printf("   *                       *             \n");
+	printf("     *                    *            \n");
+	printf("        *             *              \n");
+	printf("           *       *           \n");
+	printf("              ***                            \n");
+}
